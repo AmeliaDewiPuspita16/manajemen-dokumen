5438dc90f66f4c3f2a06dd5fb341bde5e9c872d6
Amelia Dewi Puspita <ameliapita01@gmail.com>
2026-07-31 17:35:01 +0700

    Mengubah konfigurasi database ke mysql online (TiDB)

--- config/conn.php
+++ config/conn.php
@@ -1,9 +1,35 @@
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "database_smd");
 
-	// cek koneksi
-	if(mysqli_connect_errno()) {
-		echo "Koneksi database gagal : ". mysqli_connect_errno();
-	}
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$user = "2Xtu7Gfm51rUyqA.root";
+$password = "LhhiEgeA9MDRnRMs";
+$database = "database_smd";
+$port = 4000;
+
+$conn = mysqli_init();
+
+mysqli_ssl_set(
+    $conn,
+    NULL,
+    NULL,
+    "C:/Users/Lenovo/Downloads/isrgrootx1.pem",
+    NULL,
+    NULL
+);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $password,
+    $database,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
 
 ?>
