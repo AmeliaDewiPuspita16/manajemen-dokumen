aa035adf522f226d97689289d0dd3fa341649cb9
Amelia Dewi Puspita <ameliapita01@gmail.com>
2026-07-31 18:31:29 +0700

    fix session login

--- config/auth.php
+++ config/auth.php
@@ -1,51 +1,52 @@
 <?php
-    session_start();
-    include '../config/conn.php';
+session_start();
+include '../config/conn.php';
 
 
-    if(isset($_POST['login'])){
-        if ($stmt = $conn->prepare('SELECT username, password FROM user WHERE username = ?')) {
-            $stmt->bind_param('s', $_POST['username']);
-            $stmt->execute();
-            $stmt->store_result();
+if(isset($_POST['login'])){
 
-            if ($stmt->num_rows > 0) {
-                $stmt->bind_result($username, $password);
-                $stmt->fetch();
-               
-                if (password_verify($_POST['password'], $password)){
-                    $query = mysqli_query($conn, "SELECT * FROM `user` WHERE username = '" . $_POST['username'] . "';");
-                    $fetch = mysqli_fetch_array($query);
-                    $id_current = $fetch['id_user'];
+    if ($stmt = $conn->prepare('SELECT username, password FROM user WHERE username = ?')) {
 
-                    $role = $fetch['role'];
-                    if($role == 'Admin'){
-                        // jika login sebagai admin
-                        session_start();
-                        $_SESSION['login'] = TRUE;
-                        $_SESSION['username'] = $_POST['username'];
-                        $_SESSION['id_user'] = $id_current;
-                        header('Location: ../admin/index.php');
-                    }else{
-                        //jika login sebagai user
-                        session_start();
-                        $_SESSION['login'] = TRUE;
-                        $_SESSION['username'] = $_POST['username'];
-                        $_SESSION['id_user'] = $id_current;
-                        header('Location: ../user/index.php');
-                    }
-                    
-                } else {
-                    header('Location: ../index.php?error=2');
-                    exit;
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+
+            $stmt->bind_result($username, $password);
+            $stmt->fetch();
+
+            if (password_verify($_POST['password'], $password)){
+
+                $query = mysqli_query($conn, "SELECT * FROM `user` WHERE username = '" . $_POST['username'] . "';");
+                $fetch = mysqli_fetch_array($query);
+
+                $id_current = $fetch['id_user'];
+                $role = $fetch['role'];
+
+                $_SESSION['login'] = TRUE;
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['id_user'] = $id_current;
+
+                if($role == 'Admin'){
+                    header('Location: ../admin/index.php');
+                }else{
+                    header('Location: ../user/index.php');
                 }
+
+                exit;
+
             } else {
-                header('Location: ../index.php?error=3');
+                header('Location: ../index.php?error=2');
                 exit;
             }
-            
-            $stmt->close();
-        }
-    }
-?>
 
+        } else {
+            header('Location: ../index.php?error=3');
+            exit;
+        }
+
+        $stmt->close();
+    }
+}
+?>
